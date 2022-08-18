dac18dfa4cdc285b56617e9d83f3b11b8ec40e8f
zaza mirotadze <mirotadzezaza1@gmail.com>
2022-08-18 20:28:17 +0400

    add async and wait  method

--- src/App.js
+++ src/App.js
@@ -5,24 +5,20 @@ import React from "react"
 function App() {
   const [object2, setObject2] = React.useState("")
   React.useEffect(()=>{
-    const xhr =  new XMLHttpRequest()
-    xhr.open("Get", "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita", true)
-    xhr.send()
-    xhr.onload=function(){
-        if(xhr.status === 200){
-            let object1 = JSON.parse(this.response)
-            console.log(object1.drinks[0].strDrink)
-            {setTimeout(() => {
-            setObject2(object1.drinks.map(function(elements){
-              console.log(elements)
-              return (<div key={elements.idDrink} className="element-div">
-                      <h4>{elements.strDrink}</h4>
-                      <img src={elements.strDrinkThumb}/>
-                      <p>{elements.strInstructions}</p>
-                    </div>)
-            }))  }, 2000)}
-        }
+    async function getData(){
+      const response = await  fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+      const object1 = await response.json()
+      {setTimeout(() => {
+      setObject2(object1.drinks.map(function(elements){
+        return (<div key={elements.idDrink} className="element-div">
+                <h4>{elements.strDrink}</h4>
+                <img src={elements.strDrinkThumb}/>
+                <p>{elements.strInstructions}</p>
+              </div>)
+      })) }, 2000)}
+      
     }
+    getData()
   },[])
   return (
     <div className="App">
